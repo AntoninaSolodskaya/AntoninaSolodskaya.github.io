25584ac9294751ead50bdffecee23be88ea3a5e3
Antonina Solodskaya <mailstonya@gmail.com>
2019-03-11 11:09:48 +0200

    hover changes

--- gulp-page/gulpfile.js
+++ gulp-page/gulpfile.js
@@ -4,11 +4,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
  
 gulp.task('sass', function () {
-  return gulp.src('./src/sass/**/*.scss')
+  return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./src/css'));
+    .pipe(gulp.dest('./css'));
 });
  
 gulp.task('sass:watch', function () {
-  gulp.watch('./src/sass/**/*.scss', ['sass']);
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
